5d20fd5000119af1b7ae4c7aad88ead7cc876542
Robbie Jaeger <jaeger.rob@gmail.com>
2018-02-01 11:39:42 -0700

    Add api test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,6 +3,10 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
+const environment = process.env.NODE_ENV;
+const configuration = require('../knexfile')[environment];
+const knex = require('knex')(configuration);
+
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
@@ -21,5 +25,30 @@ describe('Client Routes', () => {
 });
 
 describe('API Routes', () => {
+  beforeEach(() => {
+    knex.migrate.rollback()
+    .then(function() {
+      knex.migrate.latest()
+      .then(function() {
+        return knex.seed.run()
+      });
+    });
+  });
 
+  describe('GET /api/v1/penguins', () => {
+    it('should return all of the penguins', () => {
+      return chai.request(server)
+      .get('/api/v1/penguins')
+      .then(response => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(4);
+        response.body[0].should.have.property('species');
+      })
+      .catch(err => {
+        throw err;
+      });
+    });
+  });
 });
